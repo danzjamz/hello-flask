8f2875e6b4fec5b1f31ef6e7fbd8dcdb89017ba8
danzjamz <djl.hovley@gmail.com>
2020-01-27 16:05:23 -0700

    added functionality for sqlite db with basic guide schema; no routes

--- app.py
+++ app.py
@@ -3,12 +3,39 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
 
+# after code written, in a repl -> from app import db -> db.create_all() initializes the db
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello World'
+# @app.route('/')
+# def hello():
+#     return 'Hello World'
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+class Guide(db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary key makes it automatically incrememnting
+    title = db.Column(db.String(100), unique=False)
+    content = db.Column(db.String(144), unique=False)
+
+    def __init__(self, title, content):
+        self.title = title
+        self.content = content
+
+class GuideSchema(ma.Schema):
+    class Meta:
+        fields = ('title', 'content')
+
+
+guide_schema = GuideSchema()
+guides_schema = GuideSchema(many=True)
+
+    
 
 
 if __name__ == '__main__':
     app.run(debug = True)
+
--- readme.md
+++ readme.md
@@ -1,3 +1,9 @@
 # Flask App
 
-# built during Bottega
+built during Bottega
+
+Flask SQLAlchemy Documentation: http://flask-sqlalchemy.pocoo.org/2.3/
+
+Flask-Marshmallow Documentation: http://flask-marshmallow.readthedocs.io/en/latest/
+
+Marshmallow-SQLAlchemy Documentation: https://marshmallow-sqlalchemy.readthedocs.io/en/latest/
