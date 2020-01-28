8411ae03bbc22419ad539c6851c8d5d027a20a52
danzjamz <djl.hovley@gmail.com>
2020-01-28 10:58:25 -0700

    added GET a guide route

--- app.py
+++ app.py
@@ -56,6 +56,13 @@ def get_guides():
     return jsonify(result)
 
 
+# endpoint to query a guide
+@app.route('/guide/<id>', methods=["GET"])
+def get_guide(id):
+    guide = Guide.query.get(id)
+    return guide_schema.jsonify(guide)
+
+
 if __name__ == '__main__':
     app.run(debug = True)
 
