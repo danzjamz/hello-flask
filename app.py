5f2b649f5963e4b6b839f4eb3eca5ae094f60e39
danzjamz <djl.hovley@gmail.com>
2020-01-28 13:12:03 -0700

    added DELETE for guide

--- app.py
+++ app.py
@@ -77,6 +77,16 @@ def guid_update(id):
     return guide_schema.jsonify(guide)
 
 
+# Endpoint for deleting a guide
+@app.route('/guide/<id>', methods=["DELETE"])
+def guide_delete(id):
+    guide = Guide.query.get(id)
+    db.session.delete(guide)
+    db.session.commit()
+
+    return guide_schema.jsonify(guide)
+
+
 if __name__ == '__main__':
     app.run(debug = True)
 
